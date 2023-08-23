34c3d05f7c15e4656a37fe5943dc42f856e52362
Adam <adamcegielka@tlen.pl>
2023-08-23 15:46:54 +0200

    tests: add next Steps in `Login`

--- src/test/java/Steps/Login.java
+++ src/test/java/Steps/Login.java
@@ -3,32 +3,40 @@ package Steps;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.junit.Assert;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Login {
 
+    WebDriver driver;
+
     @Given("User is on the login page")
     public void user_is_on_the_login_page() {
         System.out.println("Step 1");
-        WebDriver driver = new ChromeDriver();
+        driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.navigate().to("https://the-internet.herokuapp.com/login");
     }
     @When("User enters a valid username")
     public void user_enters_a_valid_username() {
         System.out.println("Step 2");
+        driver.findElement(By.id("username")).sendKeys("tomsmith");
     }
     @When("User enters correct password")
     public void user_enters_correct_password() {
         System.out.println("Step 3");
+        driver.findElement(By.name("password")).sendKeys("SuperSecretPassword!");
     }
     @When("User clicks Login button")
     public void user_clicks_login_button() {
         System.out.println("Step 4");
+        driver.findElement(By.tagName("button")).click();
     }
     @Then("User has successfully logged in")
     public void user_has_successfully_logged_in() {
         System.out.println("Step 5");
+        Assert.assertEquals("https://the-internet.herokuapp.com/secure", driver.getCurrentUrl());
     }
 }
